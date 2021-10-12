e3298571bf5b53601fb8fd91f2eb8e85c8227466
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-10-12 15:00:49 +0600

    routes updated with category,product,employee,supplier

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -21,6 +21,12 @@ let storecategory = require('./components/category/create.vue').default;
 let category = require('./components/category/index.vue').default;
 let editcategory = require('./components/category/edit.vue').default;
 
+//Product Componets
+
+let storeproduct = require('./components/product/create.vue').default;
+let product = require('./components/product/index.vue').default;
+let editproduct = require('./components/product/edit.vue').default;
+
 export const routes = [
     { path: '/', component: login, name: '/' },
     { path: '/register', component: register, name:'register' },
@@ -42,5 +48,11 @@ export const routes = [
     { path: '/store-category', component: storecategory, name:'store-category'},
     { path: '/category', component: category, name:'category'},
     { path: '/edit-category/:id', component: editcategory, name:'edit-category'},
+
+    //Product Routes
+
+    { path: '/store-product', component: storeproduct, name:'store-product'},
+    { path: '/product', component: product, name:'product'},
+    { path: '/edit-product/:id', component: editproduct, name:'edit-product'},
     
 ]
