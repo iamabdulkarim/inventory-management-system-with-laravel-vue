5e6910712a927c5e5c7f1e7bd3c46cb28cc0fcdb
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-10-16 00:52:59 +0600

    footer add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -356,7 +356,22 @@
           </div>
           <!---Container Fluid-->
         </div>
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>copyright &copy; <script>
+                  document.write(new Date().getFullYear());
+                </script> - developed by
+                <b><a href="https://github.com/iamabdulkarim" target="_blank">Abdul Karim</a></b>
+              </span>
+            </div>
+          </div>
+        </footer>
+        <!-- Footer -->
       </div>
+
+
     </div>
   </div>
 
