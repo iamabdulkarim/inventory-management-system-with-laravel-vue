7ff81f7879ebcaf205734c07c97f69e36f5956aa
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-10-18 18:40:17 +0600

    store data in controller for employee salary

--- app/Http/Controllers/Api/SalaryController.php
+++ app/Http/Controllers/Api/SalaryController.php
@@ -4,8 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DB;
 
 class SalaryController extends Controller
 {
-    //
+    public function Paid(Request $request, $id)
+    {
+        $validateData = $request->validate([
+            'salary_month' => 'required',
+        ]);
+
+        $month = $request->salary_month;
+        $check = DB::table('salaries')->where('employee_id', $id)->where('salary_month', $month)->first();
+        if ($check) {
+            return response()->json('Salary already Paid');
+        } else {
+            $data = array();
+            $data['employee_id'] = $id;
+            $data['amount'] = $request->sallery;
+            $data['salary_date'] = date('d/m/Y');
+            $data['salary_month'] = $month;
+            $data['salary_year'] = date('Y');
+            DB::table('salaries')->insert($data);
+        }
+    }
 }
