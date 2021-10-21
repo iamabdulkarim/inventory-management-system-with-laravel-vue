97f9759b8412aa1d280949e83d82e4df3449f855
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-10-21 19:18:54 +0600

    edit Stock routes for  quantity

--- routes/api.php
+++ routes/api.php
@@ -31,3 +31,6 @@ Route::Get('/salary', 'Api\SalaryController@AllSalary');
 Route::Get('/salary/view/{id}', 'Api\SalaryController@ViewSalary');
 Route::Get('/edit/salary/{id}', 'Api\SalaryController@EditSalary');
 Route::Post('/salary/update/{id}', 'Api\SalaryController@SalaryUpdate');
+
+// stock
+Route::Post('/stock/update/{id}', 'Api\ProductController@StockUpdate');
