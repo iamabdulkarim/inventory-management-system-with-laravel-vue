2b72bbe58ba16a6f75a447e91b2e7310ba345d00
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-10-22 12:12:31 +0600

    Customer store and index data in CustomerController  for customer crud

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Customer;
+use Image;
+use DB;
 
 class CustomerController extends Controller
 {
@@ -14,7 +17,8 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        $customer = Customer::all();
+        return response()->json($customer);
     }
 
 
@@ -26,7 +30,42 @@ class CustomerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validateData = $request->validate([
+            'name' => 'required|unique:customers|max:255',
+            'email' => 'required',
+            'phone' => 'required|unique:customers',
+
+        ]);
+
+        if ($request->photo) {
+            $position = strpos($request->photo, ';');
+            $sub = substr($request->photo, 0, $position);
+            $ext = explode('/', $sub)[1];
+
+            $name = time() . "." . $ext;
+            $img = Image::make($request->photo)->resize(240, 200);
+            $upload_path = 'backend/customer/';
+            $image_url = $upload_path . $name;
+            $img->save($image_url);
+
+            $customer = new Customer;
+            $customer->name = $request->name;
+            $customer->email = $request->email;
+            $customer->phone = $request->phone;
+            $customer->address = $request->address;
+            $customer->photo = $image_url;
+            $customer->save();
+        } else {
+            $customer = new Customer;
+            $customer->name = $request->name;
+            $customer->email = $request->email;
+            $customer->phone = $request->phone;
+            $customer->address = $request->address;
+
+
+
+            $customer->save();
+        }
     }
 
     /**
