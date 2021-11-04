328bc01e540f6ae3788aa22751a16ae10d5c95b7
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-11-04 18:39:53 +0600

    controller for order part

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -10,10 +10,11 @@ class OrderController extends Controller
 {
     public function TodayOrder()
     {
-        $data = date('d/m/Y');
+
+        // $data = date('d/m/Y');
         $order = DB::table('orders')
             ->join('customers', 'orders.customer_id', 'customers.id')
-            ->where('order_date', $data)
+            // ->where('order_date', $data)
             ->select('customers.name', 'orders.*')
             ->orderBy('orders.id', 'DESC')->get();
         return response()->json($order);
