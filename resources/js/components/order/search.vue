7b2670030f2ad36f05827dd166404a3f26ac1f3a
iamabdulkarim <abdulkarim34.fk@gmail.com>
2021-11-06 23:57:04 +0600

    searh showing data for order details

--- resources/js/components/order/search.vue
+++ resources/js/components/order/search.vue
@@ -1,3 +1,5 @@
+ 
+
 <template>
   <div>
     <div class="row">
@@ -22,13 +24,13 @@
                       <div class="form-row">
                         <div class="col-md-12">
                           <label for="exampleFormControlTextarea1"
-                            ><b>Search By Date</b></label
+                            ><b>Search By Date </b></label
                           >
                           <input
                             type="date"
                             class="form-control"
                             id="exampleInputFirstName"
-                            required
+                            required=""
                             v-model="date"
                           />
                         </div>
@@ -49,10 +51,109 @@
             </div>
           </div>
         </div>
+
+        <div class="row">
+          <div class="col-lg-12 mb-4">
+            <!-- Simple Tables -->
+            <div class="card">
+              <div
+                class="
+                  card-header
+                  py-3
+                  d-flex
+                  flex-row
+                  align-items-center
+                  justify-content-between
+                "
+              >
+                <h6 class="m-0 font-weight-bold text-primary">Order Details</h6>
+              </div>
+              <div class="table-responsive">
+                <table class="table align-items-center table-flush">
+                  <thead class="thead-light">
+                    <tr>
+                      <th>Product Name</th>
+                      <th>Qty</th>
+                      <th>SubTotal</th>
+                      <th>Vat</th>
+                      <th>Total</th>
+                      <th>Pay</th>
+                      <th>Due</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="order in orders">
+                      <td>{{ order.name }}</td>
+                      <td>{{ order.qty }}</td>
+                      <td>{{ order.sub_total }}</td>
+                      <td>{{ order.vat }}</td>
+                      <td>{{ order.total }} $</td>
+                      <td>{{ order.pay }} $</td>
+                      <td>{{ order.due }} $</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="card-footer"></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12 mb-4">
+            <!-- Simple Tables -->
+            <div class="card">
+              <div
+                class="
+                  card-header
+                  py-3
+                  d-flex
+                  flex-row
+                  align-items-center
+                  justify-content-between
+                "
+              >
+                <h6 class="m-0 font-weight-bold text-primary">Order Details</h6>
+              </div>
+              <div class="table-responsive">
+                <table class="table align-items-center table-flush">
+                  <thead class="thead-light">
+                    <tr>
+                      <th>Product Name</th>
+                      <th>Qty</th>
+                      <th>Sub Total</th>
+                      <th>Vat</th>
+                      <th>Total</th>
+                      <th>Pay</th>
+                      <th>Due</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="order in orders">
+                      <td>{{ order.name }}</td>
+                      <td>{{ order.qty }}</td>
+                      <td>
+                        {{ order.sub_total }}
+                      </td>
+                      <td>{{ order.vat }}</td>
+                      <td>{{ order.total }}</td>
+                      <td>{{ order.pay }} $</td>
+                      <td>{{ order.due }} $</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="card-footer"></div>
+            </div>
+          </div>
+        </div>
+        <!--Row-->
       </div>
     </div>
   </div>
 </template>
+
+
 
 <script type="text/javascript">
 export default {
@@ -71,8 +172,8 @@ export default {
 
   methods: {
     searchDate() {
-      axios;
-      var data = { date: this.date }
+      var data = { date: this.date };
+      axios
         .post("/api/search/order/", data)
         .then(({ data }) => (this.orders = data))
         .catch((error) => (this.errors = error.response.data.errors));
@@ -81,4 +182,6 @@ export default {
 };
 </script>
 
-<style type="text/css"></style>
+
+<style type="text/css">
+</style>
